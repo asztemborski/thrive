329f52b5353a7c61b6e12291d187f80699e57862
Igor Asztemborski <igor.asztemborski@gmail.com>
2024-08-16 20:03:31 +0200

    nest ddd & utilities libraries

---
+++ packages/nest-ddd/src/aggregate-root.base.ts
@@ -0,0 +1,31 @@
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { EntityBase } from "./entity.base";
+import { DomainEvent } from "./domain-event.base";
+
+export abstract class AggregateRoot<
+  TEntityProps,
+> extends EntityBase<TEntityProps> {
+  private readonly _domainEvents: DomainEvent[] = [];
+
+  protected addEvent(domainEvent: DomainEvent): void {
+    this._domainEvents.push(domainEvent);
+  }
+
+  clearEvents(): void {
+    this._domainEvents.length = 0;
+  }
+
+  async publishEvents(eventEmitter: EventEmitter2): Promise<void> {
+    await Promise.all(
+      this.domainEvents.map((event) =>
+        eventEmitter.emitAsync(event.constructor.name, event),
+      ),
+    );
+
+    this.clearEvents();
+  }
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvents;
+  }
+}
