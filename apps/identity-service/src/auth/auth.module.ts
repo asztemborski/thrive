143393793987c5387b4d1b37e74242e59f95cd2b
Igor Asztemborski <igor.asztemborski@gmail.com>
2024-08-18 22:32:52 +0200

    added traefik reverse proxy

--- apps/identity-service/src/auth/auth.module.ts
+++ apps/identity-service/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { JwtModule } from '@nestjs/jwt';
 import { TypedConfigModule } from 'nest-typed-config';
@@ -7,10 +7,17 @@ import { configOptions } from './config/auth.config';
 import { PublicAuthController } from './auth.controller';
 import { serviceProviders } from './services';
 import { commandHandlers } from './commands';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypedConfigModule.forRoot(configOptions), CqrsModule, JwtModule],
+  imports: [
+    TypedConfigModule.forRoot(configOptions),
+    CqrsModule,
+    JwtModule,
+    forwardRef(() => UserModule),
+  ],
   controllers: [PublicAuthController],
   providers: [...serviceProviders, ...commandHandlers],
+  exports: [...serviceProviders],
 })
 export class AuthModule {}
--- apps/identity-service/src/user/user.module.ts
+++ apps/identity-service/src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PublicUserController } from './user.controller';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypedConfigModule } from 'nest-typed-config';
@@ -6,10 +6,12 @@ import { configOptions } from './config/user.config';
 import { commandHandlers } from './commands';
 import { serviceProviders } from './services';
 import { repositoryProviders } from './database';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypedConfigModule.forRoot(configOptions), CqrsModule],
+  imports: [TypedConfigModule.forRoot(configOptions), CqrsModule, forwardRef(() => AuthModule)],
   controllers: [PublicUserController],
   providers: [...commandHandlers, ...serviceProviders, ...repositoryProviders],
+  exports: [...serviceProviders, ...repositoryProviders],
 })
 export class UserModule {}
